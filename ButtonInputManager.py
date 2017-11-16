83ffa6e6caf355b30ae9152a712d784f74914f78
Riyaan Bakhda <riyaanbakhda2@gmail.com>
2017-11-16 19:45:55 +0500

    Fixed Button Configuraions

--- ButtonInputManager.py
+++ ButtonInputManager.py
@@ -5,8 +5,8 @@ from threading import Thread
 GPIO.setmode(GPIO.BCM)
 class ButtonInputManager(Thread):
     
-    N1 = 24
-    N2 = 25
+    N1 = 7
+    N2 = 5
     N3 = 4
     N4 = 14
     N5 = 15
@@ -15,12 +15,12 @@ class ButtonInputManager(Thread):
     N8 = 27
     N9 = 22
     N10 = 23
-    N11 = 10
-    N12 = 9
-    N13 = 11
-    N14 = 8
-    N15 = 7
-    N16 = 5
+    N11 = 24
+    N12 = 10
+    N13 = 9
+    N14 = 11
+    N15 = 25
+    N16 = 8
     N = [N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, N12, N13, N14, N15, N16]
     
     def __init__(self):
@@ -36,7 +36,7 @@ class ButtonInputManager(Thread):
             if state != 0:
                 n = int(math.log(state, 2)) + 1
                 self.add(n)
-                time.sleep(0.4)
+                #time.sleep(0.4)
 
     def pop(self):
         return self.queue.pop(0)
@@ -45,9 +45,12 @@ class ButtonInputManager(Thread):
         self.queue.append(number)
 
     def getButtonStates(self):
+        stri = ""
         number = 0
         for n in range(len(ButtonInputManager.N)):
+            stri += str(GPIO.input(ButtonInputManager.N[n]))
             number += (GPIO.input(ButtonInputManager.N[n]) * (2**n))
+        print(stri)
         return number
     
     def clear(self):
@@ -63,9 +66,8 @@ class BreathInputManager:
             print(GPIO.input(6))
 
 if __name__ == "__main__":
-    """BIM = ButtonInputManager()
-    while True:
-        print(BIM.queue)
-    """
-    BreathInputManager()
+    BIM = ButtonInputManager()
+    #while True:
+    #    print(BIM.queue)
+    
     
